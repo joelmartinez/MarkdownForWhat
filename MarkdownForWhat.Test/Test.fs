31318d7dbc3ec72247c83764e92bd7795b4fa125
Joel Martinez <joelmartinez@gmail.com>
2014-07-22 09:48:50 -0400

    Initial Commit ... WIP

---
+++ MarkdownForWhat.Test/Test.fs
@@ -0,0 +1,14 @@
+﻿namespace MarkdownForWhat.Test
+open System
+open NUnit.Framework
+open MarkdownForWhat
+
+[<TestFixture>]
+type Test() = 
+
+    [<Test>]
+    member x.TestCase() =
+        let s = new HtmlToMarkdown()
+        None
+        //Assert.AreEqual("F#", s.Parse("F#"));
+
---
+++ MarkdownForWhat/Component1.fs
@@ -0,0 +1,21 @@
+﻿namespace MarkdownForWhat
+
+open HtmlAgilityPack
+open System.Linq
+
+
+type HtmlToMarkdown() = 
+    
+    let walk (node:HtmlNode) = 
+        node.ChildNodes.ToArray()
+         
+
+    member this.Convert (html:string) =
+        let doc = new HtmlDocument()
+        doc.LoadHtml(html)
+        //doc.DocumentNode.ChildNodes.ToArray()
+        //()
+    
+
+
+
