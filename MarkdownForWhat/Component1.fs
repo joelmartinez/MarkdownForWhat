333209fb72350d62924a4724752d91def6854a81
Joel Martinez <joelmartinez@gmail.com>
2014-09-04 17:36:14 -0400

    Walk method now handling the different node types

--- MarkdownForWhat/Component1.fs
+++ MarkdownForWhat/Component1.fs
@@ -5,16 +5,54 @@ open System.Linq
 open System.Text
 
 type HtmlToMarkdown() = 
+    let blockElements = ["table";
+            "p";
+            "div";
+            "h1"; "h2"; "h3"; "h4"; "h5"; "h6";
+            "ol"; "ul";
+            "pre"; 
+            "address";
+            "blockquote";
+            "center";
+            "dl";
+            "fieldset";
+            "form";
+            "menu";
+            "body"]
+
     let builder = new StringBuilder()
 
-    let walk (node:HtmlNode) = 
-         builder.Append node.InnerText |> ignore
+
+    let handleBlock (node:HtmlNode) =
+        builder.Append node.InnerText |> ignore
+
+    let handleElement (node:HtmlNode) =
+        builder.Append node.InnerText |> ignore
+
+    let rec walk (node:HtmlNode) = 
+        let ns = node.ChildNodes.ToArray() |> Array.toList
+
+        let isblock = List.exists (fun e -> e = node.Name) blockElements 
+
+        match node.NodeType with
+        | HtmlNodeType.Document -> 
+            List.iter (fun n -> walk n) ns
+        | HtmlNodeType.Element -> 
+            match isblock with
+            | true -> handleBlock node
+            | _ -> handleElement node
+        | HtmlNodeType.Text -> 
+            builder.Append node.InnerText |> ignore
+        | _ -> 
+            builder.Append node.OuterHtml |> ignore
+
 
     member this.Convert (html:string) =
         let doc = new HtmlDocument()
         doc.LoadHtml(html)
         walk doc.DocumentNode
         builder.ToString()
+
     
 
 
