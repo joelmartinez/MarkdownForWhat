edbd76a9e02ce6287bedecc1d0f6a9ededa0ffe1
Joel Martinez <joelmartinez@gmail.com>
2014-10-15 09:19:52 -0400

    * DOMTest.fs: Added new unit test for two paragraph parsing
    * MarkdownForWhat.sln:

--- MarkdownForWhat.Test/DOMTest.fs
+++ MarkdownForWhat.Test/DOMTest.fs
@@ -18,3 +18,18 @@ type DOMTest() =
         let text = p.children.Head :?>MarkdownText
         Assert.AreEqual("what", text.value)
 
+    [<Test>]
+    member x.paragraph_two() =
+        let s = new MarkdownParser()
+
+        let x = s.Parse "<p>for</p>\n<p>what</p>"
+
+        let xt:MarkdownContainer = x :?> MarkdownContainer
+        let p = xt.children.Head :?> MarkdownParagraph
+        let text = p.children.Head :?> MarkdownText
+        Assert.AreEqual("for", text.value)
+
+        let p2 = xt.children |> List.reduce (fun _ x -> x)  :?> MarkdownParagraph
+        let text2 = p2.children.Head :?> MarkdownText
+        Assert.AreEqual("what", text2.value)
+
